c3987a0a64f3d799b266277a8a2dce752166f74d
FelpFS1 <phff800@gmail.com>
2025-03-12 20:14:19 -0300

    feat: implements todo entity

--- src/main/java/com/felipefariasdev/simple_todolist/entity/Todo.java
+++ src/main/java/com/felipefariasdev/simple_todolist/entity/Todo.java
@@ -1,4 +1,46 @@
 package com.felipefariasdev.simple_todolist.entity;
 
-public class Todo {
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_todo")
+public class Todo implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private Boolean isCompleted;
+    private Integer priority;
+
+    public Todo(String description, Boolean isCompleted,  Integer priority) {
+        this.isCompleted = isCompleted;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public Boolean getCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(Boolean completed) {
+        isCompleted = completed;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
