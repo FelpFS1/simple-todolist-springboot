676f51b421b4dc25aa14310fe05e9773f3f4cb5c
FelpFS1 <phff800@gmail.com>
2025-03-12 19:54:55 -0300

    create project

---
+++ src/main/java/com/felipefariasdev/simple_todolist/SimpleTodolistApplication.java
@@ -0,0 +1,13 @@
+package com.felipefariasdev.simple_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTodolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipefariasdev/simple_todolist/SimpleTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipefariasdev.simple_todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
