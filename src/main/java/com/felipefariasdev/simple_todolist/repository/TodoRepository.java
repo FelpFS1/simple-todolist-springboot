62600d8a9c0e90274e4fa0ccddeefb9760ea31bb
FelpFS1 <phff800@gmail.com>
2025-03-12 21:05:32 -0300

    feat: implements todo repository and todo service operations

--- src/main/java/com/felipefariasdev/simple_todolist/repository/TodoRepository.java
+++ src/main/java/com/felipefariasdev/simple_todolist/repository/TodoRepository.java
@@ -1,4 +1,7 @@
 package com.felipefariasdev.simple_todolist.repository;
 
-public class TodoRepository {
+import com.felipefariasdev.simple_todolist.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo,Long> {
 }
--- src/main/java/com/felipefariasdev/simple_todolist/service/TodoService.java
+++ src/main/java/com/felipefariasdev/simple_todolist/service/TodoService.java
@@ -1,4 +1,35 @@
 package com.felipefariasdev.simple_todolist.service;
 
+import com.felipefariasdev.simple_todolist.entity.Todo;
+import com.felipefariasdev.simple_todolist.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TodoService {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    public List<Todo> create(Todo todo){
+        todoRepository.save(todo);
+        return findAll();
+    };
+
+    public List<Todo> findAll(){
+        return todoRepository.findAll();
+    }
+
+    public List<Todo> update(Todo todo){
+        todoRepository.save(todo);
+        return findAll();
+    }
+
+    public List<Todo> delete(Long id){
+        todoRepository.deleteById(id);
+        return findAll();
+    }
+
 }
