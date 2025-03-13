8724a71bf4c942c3249dc2f3ae3f2a63cb8dc170
FelpFS1 <phff800@gmail.com>
2025-03-13 16:54:39 -0300

    feat: implement todo controller

--- src/main/java/com/felipefariasdev/simple_todolist/controller/TodoController.java
+++ src/main/java/com/felipefariasdev/simple_todolist/controller/TodoController.java
@@ -1,4 +1,34 @@
 package com.felipefariasdev.simple_todolist.controller;
 
+import com.felipefariasdev.simple_todolist.entity.Todo;
+import com.felipefariasdev.simple_todolist.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/todos")
 public class TodoController {
-}
+    @Autowired
+    private TodoService todoService;
+    @PostMapping
+    public List<Todo> create(@RequestBody Todo todo){
+        return todoService.create(todo);
+    }
+
+    @GetMapping
+    public List<Todo> findAll(){
+        return todoService.findAll();
+    }
+
+    @PutMapping
+    public List<Todo> update(@RequestBody Todo todo){
+        return todoService.update(todo);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public List<Todo> delete(@PathVariable("id") long id){
+        return todoService.delete(id);
+    }
+ }
